1eb0aa49afbd01cb807f9136fbfa683b1f997fac
brian stilphen <bstilphen@gmail.com>
2016-04-16 02:53:21 +0000

    boot.sh fix db seed

--- boot.sh
+++ boot.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 
 if [ ! -f .secret ]; then
     rake secret > .secret
-    rake db:seed
+    #rake db:seed
 fi
 
 value=`cat .secret`
@@ -14,10 +14,10 @@ export SECRET_KEY_BASE=$value
 value2=`ip route | awk 'NR==1 {print $3}'`
 export HOST_GATEWAY=$value2
 
-#export HOST_IP=${HOST_IP:-172.17.42.1}
+#export HOST_IP=${HOST_IP:-172.17.0.1}
 #export ETCD=$HOST_IP:4001
 
-echo "[contanko] booting container. : $SECRET_KEY_BASE"
+echo "[contanko] booting container."
 
 
 # Start redis
@@ -27,5 +27,7 @@ bundle exec sidekiq &
 
 rails server -b 0.0.0.0
 
-# Tail all nginx log files
+rake db:seed
+
+# Tail all log files
 tail -f /usr/src/app/log/*.log
